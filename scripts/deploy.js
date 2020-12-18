d02bbae9c7fa29c5e416ac2b720bb4a8dea9e510
Gaetan Chambouvet <gct2@3ds.com>
2020-12-18 17:50:04 +0100

    Set recursive to true

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,7 +15,7 @@ const publish = async () => {
     path.join(__dirname, "..", "dist"),
     "www/bolinette.org/docs",
     {
-      recursive: false,
+      recursive: true,
       concurrency: 1,
       tick: (localPath, remotePath, error) => {
         if (error) {
