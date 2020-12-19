95a1d2b9b905836a5d006b5f70b1241f9c6feded
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2020-12-19 15:02:33 +0100

    Add loading and error hints

--- src/composables/articles.ts
+++ src/composables/articles.ts
@@ -16,18 +16,30 @@ interface Anchor {
 
 const articles = ref<Article[]>([]);
 const tag = ref("");
+const error = ref(false);
+const loading = ref(false);
 
 const fetchArticles = async () => {
-  const res = await fetch(`${process.env.API_URL}/docs`, { mode: "cors" });
-  const json = await res.json();
-  const { code, data } = json;
-  if (code !== 200) {
-    return { articles: [], tag: "" };
-  }
+  error.value = false;
+  loading.value = true;
+  try {
+    const res = await fetch(`${process.env.API_URL}/docs`, { mode: "cors" });
+    const json = await res.json();
+    const { code, data } = json;
+    if (code !== 200) {
+      error.value = true;
+      return { success: false, articles: [], tag: "" };
+    }
 
-  articles.value = data?.articles as Article[];
-  tag.value = data?.tag;
-  return { articles: articles.value, tag: tag.value };
+    articles.value = data?.articles as Article[];
+    tag.value = data?.tag;
+    return { success: true, articles: articles.value, tag: tag.value };
+  } catch {
+    error.value = true;
+    return { success: false, articles: [], tag: "" };
+  } finally {
+    loading.value = false;
+  }
 };
 
-export { articles, tag, fetchArticles };
+export { articles, tag, error, loading, fetchArticles };
